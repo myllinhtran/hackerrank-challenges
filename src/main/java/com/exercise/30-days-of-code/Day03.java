863416744a560bff63006927e25d71d3276755a4
My Linh Tran <mylinh3591@gmail.com>
2020-05-12 19:48:09 +0200

    Update Day03.java

--- src/main/java/com/exercise/30-days-of-code/Day03.java
+++ src/main/java/com/exercise/30-days-of-code/Day03.java
@@ -7,6 +7,7 @@ public class Day03 {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        
         int N = scanner.nextInt();
 
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
